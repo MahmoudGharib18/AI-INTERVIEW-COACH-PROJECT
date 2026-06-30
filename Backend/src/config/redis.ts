5b8943adbe0ce4c5ed91b803844d184dd759cd45
MahmoudGharib18 <hematety435@gmail.com>
2026-06-30 21:39:08 +0300

    Update redis.ts

--- Backend/src/config/redis.ts
+++ Backend/src/config/redis.ts
@@ -3,10 +3,15 @@ import { Redis } from "ioredis";
 
 export const redisClient = new Redis(env.REDIS_URL, {
   maxRetriesPerRequest: null, // required by BullMQ
+  lazyConnect: false,
 });
 
 redisClient.on('connect', () => {
   console.log('✅ Redis connected');
+});
+
+redisClient.on('ready', () => {
+  console.log('🚀 Redis authenticated and ready!');
 });
 
 redisClient.on('error', (err) => {
